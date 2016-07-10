4f3a574bfcf5dd56b3ff242efdbc1ccfafad5af8
Michal Guzowski <guzowski.michal@hotmail.com>
2016-07-11 01:39:22 +0200

    Add new character, arm and arm rotation logic

---
+++ 2D Tutorial/Assets/ArmRotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmRotation : MonoBehaviour {
+
+    public float rotationOffset = 90;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; //roznica pomiedzy kursorem (po zmapowaniu ekran -> swiat gry) a bohaterem
+        difference.Normalize(); //normalizacja (suma wektorow = 1)
+
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotationOffset);
+	}
+}
